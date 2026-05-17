84dc2aa8779532173f8d9dcf3b036434fc463e1d
karimerfanul-dev <karimerfanul@gmail.com>
2026-05-17 21:15:31 +0600

    initial commit

--- src/main/java/com/erfandev/taskmanager/controller/TaskController.java
+++ src/main/java/com/erfandev/taskmanager/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.erfandev.taskmanager.controller;
 
 import com.erfandev.taskmanager.entity.Task;
 import com.erfandev.taskmanager.repository.TaskRepository;
+import com.erfandev.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,49 +15,53 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
-    private TaskRepository taskRepository;
+    private TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
+    public TaskController(TaskService taskService)
+    {
+        this.taskService=taskService;
     }
 
     @GetMapping
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+    public List<Task> getAllTasks()
+    {
+        return taskService.findAllTasks();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id){
-        return taskRepository.findById(id)
+        return taskService.getTaskById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        Task newTask = taskRepository.save(task);
+        Task newTask = taskService.createTask(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
-        return taskRepository.findById(id)
-                .map(task -> {
-                    task.setTitle(updatedTask.getTitle());
-                    task.setDescription(updatedTask.getDescription());
-                    task.setCompleted(updatedTask.getCompleted());
-                    task.setCreatedAt(updatedTask.getCreatedAt());
-                    Task newTask = taskRepository.save(task);
-                    return ResponseEntity.ok(newTask);
-                })
+        return taskService.updateTask(id,updatedTask)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
-        return taskRepository.findById(id)
-                .map(task ->  {
-                    taskRepository.delete(task);
-                    return ResponseEntity.ok().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        return taskService.deleteTaskById(id)?
+                ResponseEntity.ok().build() :
+                ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/completed/{status}")
+    public List<Task> findTaskByCompletions(@PathVariable boolean status) {
+        return taskService.getCompletedTasks(status);
+    }
+
+    @GetMapping("/search")
+    public List<Task> searchTaskByTitle(@RequestParam String title) {
+        return taskService.search(title);
     }
 }
--- src/main/java/com/erfandev/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/erfandev/taskmanager/repository/TaskRepository.java
@@ -2,9 +2,19 @@ package com.erfandev.taskmanager.repository;
 
 import com.erfandev.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+ List<Task>findByCompleted(Boolean completed);
+ List<Task> findByTitleContainingIgnoreCase(String title);
+
+ @Query("SELECT t FROM Task t WHERE t.completed= :completed")
+ List<Task> findTasksByCompletionStatus(@Param("completed") boolean completed);
+
 
 }
